9723a5b5678959a0b6ac5b16c18c91e370abc24a
Aniketkalra07 <135493915+Aniketkalra07@users.noreply.github.com>
2024-07-10 12:35:21 +0530

    NAVBAR

---
+++ my_project/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () =>{
+  return(
+    <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
+        
+         <div>
+            
+            <h1 className="text-2xl font-bold">DELICIOUS FOOD</h1>
+         </div>
+         <div>
+          
+         </div>
+    </nav>
+  )
+
+}
+
+export default NavBar;
