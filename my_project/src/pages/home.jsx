76f42fb53cc6629e6b398484dbe80e7d84dbb34a
Aniketkalra07 <135493915+Aniketkalra07@users.noreply.github.com>
2024-07-10 12:36:09 +0530

    home.jsx

---
+++ my_project/src/pages/home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Menu from "../components/Menu";
+import FoodItems from "../components/FoodItems";
+import Cart from "../components/Cart";
+
+
+
+const Home = () =>{
+  return(
+    <>
+      <NavBar/>
+      <Menu/>
+      <FoodItems/>
+      <Cart/>
+    
+    
+    
+    </>
+       
+      )
+    }
+  
+
+
+
+export default Home
