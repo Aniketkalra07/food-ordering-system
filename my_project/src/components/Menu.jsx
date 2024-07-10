1e9fce9841faf5904019ae0304c9065337a33dee
Aniketkalra07 <135493915+Aniketkalra07@users.noreply.github.com>
2024-07-10 12:35:07 +0530

    my_project/src/components/Menu.jsx

---
+++ my_project/src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Menu = () =>{
+  return(
+    <div className="ml-6">
+      
+      <div className="my-6 flex gap-3">
+        <button className="px-3 py-2 bg-red-200 font-bold rounded-lg hover:bg-green-500 text-black">
+           EVENING SNACKS
+        </button>
+       
+        
+      </div>
+    </div>
+  )
+
+}
+
+export default Menu;
